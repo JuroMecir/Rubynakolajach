44887379ff97b8db27773e8a24528f90891cb026
Juraj Mecir <jurajmecir@gmail.com>
2019-04-14 12:05:44 +0200

    pridane destroyovanie vlastneho uctu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update ]
 
   def show
     @user = User.find(params[:id])
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.all
   end
 
   def current_user?(user)
@@ -44,6 +44,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Account deleted"
+    redirect_to home_url
+  end
+
   private
 
   def user_params
@@ -63,4 +69,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+
 end
+
