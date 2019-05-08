d1996c92c46a6ec2e690d1097ec762650f5a4754
Juraj Mecir <jurajmecir@gmail.com>
2019-05-08 20:28:31 +0200

    Pridane selecty

--- app/controllers/fisherman_friend_controller.rb
+++ app/controllers/fisherman_friend_controller.rb
@@ -2,6 +2,24 @@ class FishermanFriendController < ApplicationController
 
 
   def home
+    @carp_places = ActiveRecord::Base.connection.exec_query("SELECT a.name, COUNT(*) FROM catches c
+                                                    JOIN fish f on c.fish_id = f.id
+                                                    JOIN areas a on c.area_id = a.id
+                                                    WHERE c.fish_id = 313
+                                                    GROUP BY a.id
+                                                    ORDER BY count(*) DESC")
+    @Lake_Javier_fish = ActiveRecord::Base.connection.exec_query("SELECT f.name,count(*) FROM fish f
+                                                                  join catches c on f.id = c.fish_id
+                                                                  join areas a on a.id = c.area_id
+                                                                  where a.id = 60
+                                                                  group by f.id
+                                                                  order by count(*) desc")
+    @Pike_baits = ActiveRecord::Base.connection.exec_query("SELECT b.name,count(*) FROM baits b
+                                                            join catches c on b.id = c.bait_id
+                                                            join fish f on c.fish_id = f.id
+                                                            where f.name = 'Pike'
+                                                            group by b.id
+                                                            order by count(*) desc")
     if logged_in?
       @catch  = current_user.catches.build
       @feed_items = current_user.feed.paginate(page: params[:page])
