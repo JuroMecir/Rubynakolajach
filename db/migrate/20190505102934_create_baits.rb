87fc06394e0e30ac2ab85f319ba0b31abf8c132f
Juraj Mecir <jurajmecir@gmail.com>
2019-06-18 14:22:34 +0200

    upraveny vzhlad formularov

--- db/migrate/20190505102934_create_baits.rb
+++
@@ -1,9 +0,0 @@
-class CreateBaits < ActiveRecord::Migration[5.2]
-  def change
-    create_table :baits do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20190505103046_create_methodfs.rb
+++
@@ -1,9 +0,0 @@
-class CreateMethodfs < ActiveRecord::Migration[5.2]
-  def change
-    create_table :methodfs do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
